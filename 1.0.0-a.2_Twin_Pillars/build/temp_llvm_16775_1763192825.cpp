8ca211fcdedfc0c6aa974d7fee2472f6c5e1efe8
xfawa543 <1844494802@qq.com>
2025-11-19 20:32:30 +0800

    Delete 1.0.0-a.2_Twin_Pillars/build directory

--- 1.0.0-a.2_Twin_Pillars/build/temp_llvm_16775_1763192825.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-int main() {
-    std::cout << u8"你好，世界！" << std::endl;
-    std::cout << u8"114514" << std::endl;
-    std::cout << u8"HelloWorld" << std::endl;
-    return 0;
-}
